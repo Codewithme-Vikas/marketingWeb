feb7665f7f0e902133bd49128dbf642c1642f421
Vikash Gurjar <vikashnagar2025@gmail.com>
2024-01-01 20:36:10 +0530

    show popup only one time

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ document.getElementById('toggleNav').addEventListener('click', function () {
     document.getElementById('mobileNav').classList.toggle('hidden');
 });
 
-// pop container
-document.addEventListener('DOMContentLoaded', function () {
-    // Check the window width on page load
-    checkWindowWidth();
 
-    // Check the window width when the window is resized
-    window.addEventListener('resize', checkWindowWidth);
-});
+
+// pop container
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Check the window width on page load
+//     checkWindowWidth();
+
+//     // Check the window width when the window is resized
+//     window.addEventListener('resize', checkWindowWidth);
+// });
 
 function checkWindowWidth() {
     // Get the window width
@@ -27,3 +29,17 @@ function checkWindowWidth() {
 function closePopup() {
     document.getElementById('popupContainer').style.display = 'none';
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if the pop-up has already been shown in this session
+    let isPopupShown = sessionStorage.getItem('isPopupShown');
+
+    // If not, show the pop-up
+    if (!isPopupShown) {
+        checkWindowWidth(); // Assuming checkWindowWidth also checks the window width and shows the pop-up
+
+        // Set the flag in sessionStorage to indicate that the pop-up has been shown
+        sessionStorage.setItem('isPopupShown', true);
+    }
+});
